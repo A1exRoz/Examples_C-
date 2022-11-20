525555e72c081143cc6c09dcb05d344812cbbb6a
alexrozanov <alexrozans@gmail.com>
2022-11-20 22:00:23 +0300

    add some text

--- Ex000_Test/Program.cs
+++ Ex000_Test/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿DateTime nowDate = DateTime.Now;
+string someText = "Сегодня";
+int number = 924;
+Console.WriteLine(nowDate);
+Console.WriteLine(someText);
+Console.WriteLine(number);
+Console.WriteLine($"{someText} {nowDate}. Число: {number}");
+Console.WriteLine("{1}: {0:yyyy-MM-dd}, Число: {2}", nowDate, someText, number);
