4b902a80b9dd4e04a56874cf1670322220976880
alexrozanov <alexrozans@gmail.com>
2022-11-23 22:14:31 +0300

    add new example

---
+++ Ex011_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 12, 241, 32, 41, 32, 644, 72, 81, 57 };
+
+int n = array.Length;
+int find = 32;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
