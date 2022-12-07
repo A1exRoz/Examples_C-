78dc01a9774fd2f12a40d62e58e538ffeca1e32a
alexrozanov <alexrozans@gmail.com>
2022-12-07 23:25:00 +0300

    add new example

---
+++ Ex016_Recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿// Вычисление факториала
+
+int Factorial(int n)  // можно через double - так числа будут больше
+{
+ // 1! = 1
+ // 0! = 1
+ if(n == 1) return 1;
+ else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+ Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
+
+
+// Вычисление чисел Фибоначчи
+
+double Fibonacci(int n)   // как раз пример чере double
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+ Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
